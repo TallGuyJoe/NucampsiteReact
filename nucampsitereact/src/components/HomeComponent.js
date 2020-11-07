3e41ca0c82984009b238489fc02be40180bddf80
Joe <hernandezhjoe@gmail.com>
2020-11-07 02:06:08 -0800

    Redux Thunk fix

--- nucampsitereact/src/components/HomeComponent.js
+++ nucampsitereact/src/components/HomeComponent.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 
-function RenderCard({item, isloading, errMess}) {
-
-    if (isloading) {
+function RenderCard({item, isLoading, errMess}) {
+    if (isLoading) {
         return <Loading />;
     }
     if (errMess) {
-        return <h4>{errMess}</h4>
+        return <h4>{errMess}</h4>;
     }
-
     return (
         <Card>
             <CardImg src={item.image} alt={item.name} />
@@ -21,6 +19,7 @@ function RenderCard({item, isloading, errMess}) {
         </Card>
     );
 }
+
 
 function Home(props) {
     return (
