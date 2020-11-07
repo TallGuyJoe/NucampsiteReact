4a8f5533ba01580295992a5bdf5334d7472bef18
Joe <hernandezhjoe@gmail.com>
2020-11-07 00:29:47 -0800

    react-redux-form

--- nucampsitereact/src/redux/comments.js
+++ nucampsitereact/src/redux/comments.js
@@ -2,7 +2,7 @@ import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
 
-	export const Comments = (state = COMMENTS, action) => {
+export const Comments = (state = COMMENTS, action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENT:
             const comment = action.payload;
