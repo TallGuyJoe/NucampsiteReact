5a39bebb098b252783e46f5df2cf2fb92a43facf
Joe <hernandezhjoe@gmail.com>
2020-11-10 12:52:40 -0800

    Fetch handling errors

--- nucampsitereact/src/redux/ActionCreators.js
+++ nucampsitereact/src/redux/ActionCreators.js
@@ -18,8 +18,23 @@ export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
     
     return fetch(baseUrl + 'campsites')
+        .then(response => {
+                if (response.ok) {
+                        return response;
+                } else {
+                        const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                        error.response = response;
+                        throw error;
+                }
+                },
+                error => {
+                        const errMess = new Error(error.message);
+                        throw errMess;
+                }
+        )
         .then(response => response.json())
-        .then(campsites => dispatch(addCampsites(campsites)));
+        .then(campsites => dispatch(addCampsites(campsites)))
+        .catch(error => dispatch(campsitesFailed(error.message)));
 };
 
 	export const campsitesLoading = () => ({
@@ -37,9 +52,24 @@ export const fetchCampsites = () => dispatch => {
         });
 
         export const fetchComments = () => dispatch => {    
-        return fetch(baseUrl + 'comments')
-            .then(response => response.json())
-            .then(comments => dispatch(addComments(comments)));
+                return fetch(baseUrl + 'comments')
+                .then(response => {
+                        if (response.ok) {
+                            return response;
+                        } else {
+                            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                            error.response = response;
+                            throw error;
+                        }
+                    },
+                    error => {
+                        const errMess = new Error(error.message);
+                        throw errMess;
+                    }
+                )
+                .then(response => response.json())
+                .then(comments => dispatch(addComments(comments)))
+                .catch(error => dispatch(commentsFailed(error.message)));
         };
 
 	export const commentsFailed = errMess => ({
